fe02c5fdf811854190b6ca10659b565471c49c9f
Welly Shen <hivoid19@gmail.com>
2020-07-04 14:46:09 +0800

    feat: use React context API for global states

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Header, Layer } from 'grommet'
 import AddressInput from '../Components/AddressInput.jsx'
 import { Sun } from 'grommet-icons'
 
+import { Context } from '../Contexts/LatLng';
+
 const HeaderDiv = () => {
+	const { latlng } = useContext(Context);
+
 	return (
 		<Header
 			pad='xsmall'
@@ -12,6 +16,7 @@ const HeaderDiv = () => {
 			background={{ dark: 'black', light: 'white' }}>
 			<b>Your Address:</b>
 			<AddressInput />
+			<div>lat: {latlng.lat} lng: {latlng.lng}</div>
 			{/* <Moon />
 			<Sun /> */}
 		</Header>
---
+++ src/Contexts/LatLng.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+const Context = createContext({ latlng: {}, setLatlng: () => {} });
+
+const Provider = ({ children }) => {
+  const [latlng, setLatlng] = useState({});
+
+  return (
+    <Context.Provider value={{ latlng, setLatlng }}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export { Context, Provider };
